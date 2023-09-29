4e257f5bea9ee085a57a8719eb8f2041d3549227
Mustafa <2fmeh66@gmail.com>
2023-09-29 16:07:55 +0200

    @PostConstruct and @PreDestroy annotations added

--- src/main/java/com/Mustafa/myCoolApp/Rest/MyRestController.java
+++ src/main/java/com/Mustafa/myCoolApp/Rest/MyRestController.java
@@ -1,6 +1,9 @@
 package com.Mustafa.myCoolApp.Rest;
 import com.Mustafa.myCoolApp.WeatherService;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,7 +41,7 @@ public class MyRestController { // the class name as "RestController" will cause
 
     // Create an object from the interface WeatherService
     // Define a constructor for dependency injection
-    private WeatherService myWeatherService;
+    private final WeatherService myWeatherService;
 
     // The @Autowired annotation here indicates that we are using constructor injection.
     // Constructor injection means passing the dependency (in this case, a WeatherService) as a constructor parameter.
@@ -65,7 +68,28 @@ But in general, constructor injection is preferred over setter injection.
 
     @GetMapping("/weather")
     public String getWeather() {
-           return myWeatherService.getTheWeather();
-}
+        return myWeatherService.getTheWeather();
+    }
+
+    // We use PostConstruct in order to execute some code after the bean has been initialized.
+    // This is useful for performing any initialization tasks that we may need to do before the bean is ready to be used.
+    // For example, we may want to open a database connection or start a thread.
+    // here only to see the functionality of the annotation, after starting the application, we will see this message in the console.
+    @PostConstruct
+    public void init() {
+        // Initialization code here
+        System.out.println("MyRestController bean has been initialized.");
+    }
+
+    // We use PreDestroy in order to execute some code before the bean is destroyed.
+    // This is useful for releasing resources that the bean may have been holding onto.
+    // For example, you may want to close a database connection or stop a thread.
+    // here only to see the functionality of the annotation, after stopping the application, we will see this message in the console.
+    @PreDestroy
+    public void cleanup() {
+        // Cleanup code here
+        System.out.println("MyRestController bean is being destroyed.");
+    }
+
 
 }
